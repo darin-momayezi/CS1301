f8d0fd4bd81ae3b5c3b1ac554852b4fbb423bc79
darin-momayezi <77948633+darin-momayezi@users.noreply.github.com>
2022-02-21 10:35:04 -0500

    Update HW05.py

--- HW05.py
+++ HW05.py
@@ -1,5 +1,21 @@
 from calendar import weekday
-from smashData.py import *
+
+def counterPick(fighter):
+    counter = {
+            'mario':['luigi','link'],
+            'luigi':['bowser','ness'],
+            'bowser':['mario','link'],
+            'captain falcon':['fox','marth'],
+            'fox':['samus','mewtwo'],
+            'ness':['bowser','samus'],
+            'kirby':['mario','captain falcon'],
+            'samus':['pikachu','marth'],
+            'link':['fox','ness'],
+            'pikachu':['captain falcon','kirby'],
+            'marth':['luigi','mewtwo'],
+            'mewtwo':['kirby','pikachu']
+    }
+    return counter.get(fighter)
 
 
 def showToWatch(friendsFavShows, favoriteShow):
