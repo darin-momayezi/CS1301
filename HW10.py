6366c6728abaf4ff071ef660d989fdf1b94c241d
darin-momayezi <77948633+darin-momayezi@users.noreply.github.com>
2022-04-15 15:44:30 -0400

    Update HW10.py

--- HW10.py
+++ HW10.py
@@ -18,7 +18,7 @@ class Video:
 
 class Account:
 
-    def __init__(self, username, password, isPrivate):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
         self.followers = []
